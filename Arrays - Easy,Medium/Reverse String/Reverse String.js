fc60072493e53df200e14f62965c14de77c1a93e
Abiramisri2k <abiramisri2k@gmail.com>
2025-09-06 21:41:43 +0530

    First Commit

---
+++ Arrays - Easy,Medium/Reverse String/Reverse String.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let len = s.length;
+    let halflen = len/2;
+    for(let i=0; i<halflen; i++){
+        let temp = s[i];
+        s[i] = s[len-1-i];
+        s[len-1-i] = temp;
+    }
+};
+
+//Time complexity - O(n), where n = nums.length.
+//Space complexity - O(1)(constant extra space).
